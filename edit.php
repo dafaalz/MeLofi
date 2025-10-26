250f1878bb234157131f8f51a759b7a605ce81b7
dafaalz <dafaalzabiy@gmail.com>
2025-10-26 20:41:41 +0700

    added style to edit page

--- edit.php
+++ edit.php
@@ -24,30 +24,42 @@ include 'sidebar.php';
 ?>
 
 <main class="app-content">
-    <div id="container">
-        <h2 style="color: #1e1e1e;" >Edit Info Lagu</h2>
-        <br>
+    <div class="edit-container">
+        <h2 class="page-title">Edit Info Lagu</h2>
         <form action="edit_proses.php" method="post">
             <input type="hidden" name="id" value="<?php echo $row['id_lagu'];?>" required>
-            <label>Judul</label>
-            <input type="text" name="judul" value="<?php echo $row['judul'];?>" required>
-            <label>Artis</label>
-            <select name="id_artis" id="id_artis" required>
-                <option value="">-- Pilih Artis --</option>
-                <?php while($artis = mysqli_fetch_assoc($artis_result)) { ?>
-                    <option value="<?php echo $artis['id_artis']; ?>">
-                        <?php echo $artis['nama_artis']; ?>
-                    </option>
-                <?php } ?>
-            </select>
-            <label>Album</label>
-            <select name="id_album" id="id_album" required>
-                <option value="">-- Pilih Album --</option>
-            </select>
-            <input type="submit" value="Simpan Perubahan">
+
+            <div class="form-group">
+                <label for="judul">Judul</label>
+                <input type="text" id="judul" name="judul" class="form-control" value="<?php echo $row['judul'];?>" required>
+            </div>
+
+            <div class="form-group">
+                <label for="id_artis">Artis</label>
+                <select name="id_artis" id="id_artis" class="select-box" required>
+                    <option value="">-- Pilih Artis --</option>
+                    <?php while($artis = mysqli_fetch_assoc($artis_result)) { ?>
+                        <option value="<?php echo $artis['id_artis']; ?>">
+                            <?php echo $artis['nama_artis']; ?>
+                        </option>
+                    <?php } ?>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="id_album">Album</label>
+                <select name="id_album" id="id_album" class="select-box" required>
+                    <option value="">-- Pilih Album --</option>
+                </select>
+            </div>
+
+            <div class="form-actions">
+                <input type="submit" value="Simpan Perubahan" class="btn-primary">
+                <a href="adminPage.php" class="btn-secondary">Kembali ke Admin Page</a>
+            </div>
         </form>
-        <p><a href="adminPage.php">Kembali ke Admin Page</a></p>
     </div>
+
     <script>
     document.getElementById('id_artis').addEventListener('change', function() {
         var idArtis = this.value;
@@ -62,4 +74,5 @@ include 'sidebar.php';
     });
     </script>
 </main>
+
 <?php include 'footer.php'; ?>
